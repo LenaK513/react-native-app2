8c028a2f80e42fe8b6cc9e0c68a0a796d55e3a72
Olena <kryvtsovaolena@gmail.com>
2023-03-14 18:17:13 +0200

    basic routing

--- App.js
+++ App.js
@@ -7,22 +7,19 @@ import { useFonts } from "expo-font";
 import { StyleSheet, View } from "react-native";
 import RegistrationScreen from "./Screens/auth/RegistrationScreen/RegistrationScreen";
 import LoginScreen from "./Screens/auth/LoginScreen/LoginScreen";
+import Home from "./Screens/main/Home";
+import CommentsScreen from "./Screens/main/CommentsScreen";
+import CreatePostsScreen from "./Screens/main/CreatePostsScreen";
+import MapScreen from "./Screens/main/MapScreen";
+import PostsScreen from "./Screens/main/PostsScreen";
+import ProfileScreen from "./Screens/main/ProfileScreen";
 
 const AuthStack = createStackNavigator();
 const MainTab = createBottomTabNavigator();
 
-export default function App() {
-  const [fontsLoaded] = useFonts({
-    "r-bold": require("./assets/fonts/Roboto-Bold.ttf"),
-    "r-light": require("./assets/fonts/Roboto-Light.ttf"),
-    "r-regular": require("./assets/fonts/Roboto-Regular.ttf"),
-  });
-
-  if (!fontsLoaded) {
-    return null;
-  }
-  return (
-    <NavigationContainer>
+const useRoute = (isAuth) => {
+  if (!isAuth) {
+    return (
       <AuthStack.Navigator>
         <AuthStack.Screen
           options={{ headerShown: false }}
@@ -35,8 +32,33 @@ export default function App() {
           component={LoginScreen}
         />
       </AuthStack.Navigator>
-    </NavigationContainer>
+    );
+  }
+  return (
+    <MainTab.Navigator>
+      <MainTab.Screen name="Home" component={Home} />
+      <MainTab.Screen name="CommentsScreen" component={CommentsScreen} />
+      <MainTab.Screen name="CreatePostsScreen" component={CreatePostsScreen} />
+      <MainTab.Screen name="MapScreen" component={MapScreen} />
+      <MainTab.Screen name="PostsScreen" component={PostsScreen} />
+      <MainTab.Screen name="ProfileScreen" component={ProfileScreen} />
+    </MainTab.Navigator>
   );
+};
+
+export default function App() {
+  const routing = useRoute(null);
+
+  const [fontsLoaded] = useFonts({
+    "r-bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "r-light": require("./assets/fonts/Roboto-Light.ttf"),
+    "r-regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  return <NavigationContainer>{routing}</NavigationContainer>;
 }
 
 const styles = StyleSheet.create({
