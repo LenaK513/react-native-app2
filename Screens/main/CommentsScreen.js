de788ad2ee9a388bd4304bfd08986a528031bf4a
Olena <kryvtsovaolena@gmail.com>
2023-05-05 22:22:02 +0300

    Update CommentsScreen.js

--- Screens/main/CommentsScreen.js
+++ Screens/main/CommentsScreen.js
@@ -1,19 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Text,
   StyleSheet,
   View,
   TouchableOpacity,
   TextInput,
+  SafeAreaView,
+  FlatList,
 } from "react-native";
 import { useSelector } from "react-redux";
 import { db } from "../../firebase/config";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 
 const CommentsScreen = ({ route }) => {
   const { postId } = route.params;
   const [comments, setComments] = useState("");
+  const [allComments, setAllComments] = useState([]);
   const { login } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    getAllPost();
+  }, []);
 
   const inputHandler = (text) => setComments(text);
 
@@ -30,8 +37,33 @@ const CommentsScreen = ({ route }) => {
     }
   };
 
+  const getAllPost = async () => {
+    try {
+      const allPosts = await getDocs(
+        collection(db, "posts", postId, "comments")
+      );
+      setAllComments(
+        allPosts.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      );
+    } catch (error) {
+      console.log("error", error.message);
+    }
+  };
+
   return (
     <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={allComments}
+          renderItem={({ item }) => (
+            <View style={styles.commentContainer}>
+              <Text>{item.login}</Text>
+              <Text>{item.comments}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
       <TextInput
         style={styles.inputArea}
         placeholder="write a comment"
@@ -50,8 +82,15 @@ const CommentsScreen = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-end",
     backgroundColor: "#fff",
+  },
+  commentContainer: {
+    borderWidth: 1,
+    borderColor: "#20b2aa",
+    marginHorizontal: 10,
+    padding: 10,
+    marginBottom: 10,
   },
   input: {
     fontFamily: "r-regular",
